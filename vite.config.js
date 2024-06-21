cf096914da4d4bf9601f69f1fedda41fa75ed468
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-06-21 15:40:32 +0530

    update vite cofig file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000/api/v1'
+      '/api': 'https://youtube-backend-oi4y.onrender.com/api/v1'
     }
   }
 })
